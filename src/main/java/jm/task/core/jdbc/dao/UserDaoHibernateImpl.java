5da9cfe9066d7c41391c397dd2e979611a6c1ae4
alalalalka <freshvisitka@yandex.ru>
2022-08-07 17:08:05 +0300

    test

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,14 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.SessionFactory;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
+    private final SessionFactory sessionFactory = (SessionFactory) Util.getConnection();
     public UserDaoHibernateImpl() {
 
     }
